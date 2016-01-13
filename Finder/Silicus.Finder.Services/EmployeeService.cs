d4135d0afcc35ca8fd903a01b5b7646157537716
pratikpatil01 <Pratik.Patil@silicus.com>
2016-01-13 17:57:38 +0530

    EmployeeRelatedCommit

---
+++ Finder/Silicus.Finder.Services/EmployeeService.cs
@@ -0,0 +1,20 @@
+﻿using Silicus.Finder.Entities;
+using Silicus.Finder.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Silicus.Finder.Services
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly IDataContext context;
+
+        public EmployeeService(IDataContextFactory dataContextFactory)
+        {
+            this.context = dataContextFactory.Create(ConnectionType.Ip);
+        }
+    }
+}
