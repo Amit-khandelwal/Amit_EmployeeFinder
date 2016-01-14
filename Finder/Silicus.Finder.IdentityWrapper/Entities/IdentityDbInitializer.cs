cb234f23b599b0e92b66d45d7c178f0c53de64a3
shailendrabirthare <shailendra.birthare@gmail.com>
2016-01-14 10:58:05 +0530

    Refactored user and role creation code

--- Finder/Silicus.Finder.IdentityWrapper/Entities/IdentityDbInitializer.cs
+++ Finder/Silicus.Finder.IdentityWrapper/Entities/IdentityDbInitializer.cs
@@ -28,28 +28,45 @@ namespace Silicus.Finder.IdentityWrapper.Entities
             const string password = "Admin@123456";
             const string roleName = "Admin";
 
-            //Create Role Admin if it does not exist
-            var role = roleManager.FindByName(roleName);
-            if (role == null)
-            {
-                role = new IdentityRole(roleName);
-                var roleresult = roleManager.Create(role);
-            }
+            var adminRole = CreateIdentityRoleIfNotExist(roleManager, roleName);
+            var adminUser = CreateUserIfNotExist(userManager, name, password);
+            AssignRoleToUser(userManager, adminUser, adminRole);
 
-            var user = userManager.FindByName(name);
-            if (user == null)
-            {
-                user = new ApplicationUser { UserName = name, Email = name };
-                var result = userManager.Create(user, password);
-                result = userManager.SetLockoutEnabled(user.Id, false);
-            }
+            var roleUser = CreateIdentityRoleIfNotExist(roleManager, "User");
+            var userApplicationUser = CreateUserIfNotExist(userManager, "testuser@test.com", "testuser@123");
+            AssignRoleToUser(userManager, userApplicationUser, roleUser);
+        }
 
-            // Add user admin to Role Admin if not already added
+        private static void AssignRoleToUser(ApplicationUserManager userManager, ApplicationUser user, IdentityRole role)
+        {
             var rolesForUser = userManager.GetRoles(user.Id);
             if (!rolesForUser.Contains(role.Name))
             {
                 var result = userManager.AddToRole(user.Id, role.Name);
             }
+        }
+
+        private static ApplicationUser CreateUserIfNotExist(ApplicationUserManager userManager, string name, string password)
+        {
+            var user = userManager.FindByName(name);
+            if (user == null)
+            {
+                user = new ApplicationUser {UserName = name, Email = name};
+                var result = userManager.Create(user, password);
+                result = userManager.SetLockoutEnabled(user.Id, false);
+            }
+            return user;
+        }
+
+        private static IdentityRole CreateIdentityRoleIfNotExist(ApplicationRoleManager roleManager, string roleName)
+        {
+            var role = roleManager.FindByName(roleName);
+            if (role == null)
+            {
+                role = new IdentityRole(roleName);
+                roleManager.Create(role);
+            }
+            return role;
         }
     }
 }
