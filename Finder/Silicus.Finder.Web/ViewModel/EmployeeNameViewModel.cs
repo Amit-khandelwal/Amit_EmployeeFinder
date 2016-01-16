25316697aa9d64da7071394f034a2c81e875a735
rashigupta368 <rashi.gupta@silicus.com>
2016-01-16 19:56:23 +0530

    Commit Tem

---
+++ Finder/Silicus.Finder.Web/ViewModel/EmployeeNameViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Silicus.Finder.Web.ViewModel
+{
+    public class EmployeeNameViewModel
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+
+        [Required(ErrorMessage = "First Name can't be blank")]
+        [StringLength(20, ErrorMessage = "First Name should contain less than 20 characters")]
+        public string FullName{ get; set; }
+    }
+}
--- Finder/Silicus.Finder.Web/ViewModel/EmployeeViewModel.cs
+++ Finder/Silicus.Finder.Web/ViewModel/EmployeeViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,14 +10,14 @@ namespace Silicus.Finder.Web.ViewModel
     {
         public int EmployeeId { get; set; }
 
-        [Required(ErrorMessage = "First Name can't be blank")]
-        [StringLength(20, ErrorMessage = "First Name should contain less than 20 characters")]
-        public string FullName{ get; set; }
-        public string FirstName { get; set; }
-        public string MiddleName { get; set; }
-        public string LastName { get; set; }
-        public string FullName { get { return FirstName + " " + LastName; } }
-        public int CubicleLocationId { get; set; }      /
-        public int ContactId { get; set; }
+        //[Required(ErrorMessage = "First Name can't be blank")]
+        //[StringLength(20, ErrorMessage = "First Name should contain less than 20 characters")]
+        //public string FullName{ get; set; }
+        //public string FirstName { get; set; }
+        //public string MiddleName { get; set; }
+        //public string LastName { get; set; }
+        //public string FullName { get { return FirstName + " " + LastName; } }
+        //public int CubicleLocationId { get; set; }      
+        //public int ContactId { get; set; }
     }
 }
